ea8c1886576efe69f36933aa7c471b331cd7c777
Joe0829 <57712487+Joe0829@users.noreply.github.com>
2019-11-29 15:00:51 +0100

    Add files via upload

---
+++ ImagineThisWasCode.ps1
@@ -0,0 +1,30 @@
+#Imagine this would describe a step
+
+This-IsCode -Name "Code_abc"
+	Code_1
+	Code_2
+	Code_3
+
+#Description of values that are required for following steps
+
+This-IsAlsoCode -Name "Code_def"
+	Code_4
+	Code_5
+	Code_6
+
+#Description of how earlier steps were used to produce values for following step
+
+There-IsMoreCode -Name "Code_ghj" #end of line comment
+	Code_7
+	Code_8
+	Code_9
+
+#Description of output created by this code
+
+That-IsEvenMoreCode
+	This-Prints "#Cool"
+	This-Prints 'Even #cooler'
+
+<#Description of what this does to produce value -
+which happens accross multiple lines
+- and even more lines.#>
